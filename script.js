bb40cf322a377c1e5af446d766d38d6e1d06687a
Joseph Knox <jfknox3@gmail.com>
2014-07-28 08:50:07 -0700

    Mad ethe plant animate only on drop event. Still need to get the counter to work, but I have the items remove as soon as I drop them over the pipe.

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-var app = angular.module("retro-estApp", []); 
+/////////////////////////////////
+//Vanila JS
+/////////////////////////////////
 
+
+//////////////////////////////
+//Functions for the HTML DRAG AND DROP
+/////////////////////////////
+    var draggedElement;
+		function allowDrop(ev) {
+    	ev.preventDefault();
+}
+
+	function drag(ev) {
+    	draggedElement = ev.target;
+}
+
+ //////////////////////////////////
+ //Functions for the animation of the piranha plant
+ //////////////////////////////////   
+
+	function drop(ev) {
+	    ev.preventDefault();
+	    $(draggedElement).hide("slow", draggedElement.remove)
+	    $(plant).animate({ height: 150 }, 1250);
+	    $(plant).animate({ height: 100 }, 1250);
+}
+/////////////////////////////////////////
+//Angular framework
+/////////////////////////////////////////
+var app = angular.module("retro-estApp", []); 
 app.controller('profilesCtrl', function($scope) {
 	$scope.profiles = retroGameCharacters.profiles;
 	//create variables to store info
@@ -8,11 +37,11 @@ app.controller('profilesCtrl', function($scope) {
 	$scope.newYear = ""; 
 	$scope.newConsole = "";
 	$scope.newCharName = "";
-
-
-	// new function that adds items
+/////////////////////////////////
+// new function that adds items
+///////////////////////////////
 	$scope.addNewProfile = function() {
-		//check promts to see if blank 
+		if ($scope.newPic != "") {
 			$scope.profiles.push({
 				pic: $scope.newPic, 
 				gName: $scope.newGname,
@@ -20,7 +49,11 @@ app.controller('profilesCtrl', function($scope) {
 				console: $scope.newConsole, 
 				charName: $scope.newCharName,
 				removed: false
-			});	
+			})
+		}
+		else{
+			alert("Please add a valid picture!")
+		}
 			//reset the profile variables
 				$scope.newPic = "";
 				$scope.newGname = "";
@@ -28,82 +61,31 @@ app.controller('profilesCtrl', function($scope) {
 				$scope.newConsole = "";	
 				$scope.newCharName = "";	
 	};
-	
-	//make function that removes items
-	$scope.removeProfile = function () {
-		//store all of the collected items to return later
-		var deletedProfiles = $scope.profiles;
-		//remove all items from the original variable
-		$scope.profiles = [];
-		//loop through with the forEach operator
-		angular.forEach(deletedProfiles, function(profile) {
-			if(!profile.removed) {
-			//if the profile is not removed already, put it into the original variable
-				$scope.profiles.push(profile);
-			}
-		});
-	};
-
-	//create a function that returns the total amount
-
-
+///////////////////////////////////////
+//create a function that returns the total amount
+//////////////////////////////////////
 	$scope.totalProfiles = function() {
-	//total number of items
 		var remaining = $scope.profiles.length;
-		return remaining
+	return remaining
 }
-
 });
-
-//////////////////////////////
-//Functions for the HTML DRAG AND DROP
-/////////////////////////////
-  var draggedElement;
-		function allowDrop(ev) {
-    	ev.preventDefault();
-}
-
-	function drag(ev) {
-    	//ev.dataTransfer.setData("Text", ev.target.id);
-    	draggedElement = ev.target;
-}
-
-	function drop(ev) {
-	    ev.preventDefault();
-	    var data = ev.dataTransfer.getData("Text");
-	    ev.target.appendChild(draggedElement);
-}
-
- //////////////////////////////////
- //Functions for the animation of the piranha plant
- //////////////////////////////////    
+////////////////////////////////
+//Jquery framework
+///////////////////////////////
 $(document).ready(function() {
-		var $Delete = $("#delete");
-		var $plant = $("#plant");
-//create a $pics and $content variable to reduce the amount of times I access the DOM
-
-//when the mouse enters make the object bigger.
-    $Delete.mouseenter(function() {
-   		$(plant).css('height', '80px').animate({ height: 150 }, 1750);
-    }); 
-//return to normal size when mouse leaves
-    $Delete.mouseleave(function() {
-		$(plant).css('height', '150px').animate({ height: 80 }, 1750);
-		
-	});
-
+     
 
 ////////////////////////////////////////
 //Function to get the New character form to drop down
 ////////////////////////////////////////
-
 	$("#button").click(function() {
-		$(".new").show("slow");
+		$(".new").toggle("slow");
 	});
 
 	$("#submit").click(function() {
 		$(".new").hide("slow")
 	});
+
 });
 
 
